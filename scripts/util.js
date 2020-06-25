669b0ed2395e6d910310ee2be7669a8f8ab8fcde
phiman <phiman@thoughtworks.com>
2020-06-25 22:39:24 +0530

    Changed content for confrences section

--- scripts/util.js
+++ scripts/util.js
@@ -4,12 +4,18 @@ $('.menu__mobile-button, .mobile-menu__close').on('click', function () {
 });
 
 function getAge() {
-    var dob_asdate = new Date(1993, 06, 30);
-    var today = new Date();
-    var mili_dif = Math.abs(today.getTime() - dob_asdate.getTime());
-    return Math.floor(mili_dif / (1000 * 3600 * 24 * 365.25));
+    var startDate = moment("30.06.1993", "DD.MM.YYYY");
+    var endDate = moment();
+    return endDate.diff(startDate, 'years');
+}
+
+function getTimeInTW() {
+    var startDate = moment("15.01.2018", "DD.MM.YYYY");
+    var endDate = moment();
+    return `${endDate.diff(startDate, 'years')} yrs ${endDate.diff(startDate, 'months') % 24} mos`;
 }
 
 $(document).ready(function () {
     $("#spnAge").text(getAge());
+    $("#spnTimeInTW").text(getTimeInTW());
 });
